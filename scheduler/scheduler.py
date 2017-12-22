d66ecb89f21c050cbe49bbb26464460e5dc3da36
Marcel Schwittlick <lzrm@mrzl.net>
2017-12-22 06:51:04 +0100

    refactoring and adds some inline comments

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -83,10 +83,10 @@ class Scheduler(object):
             self.lock()
             self.log(script_to_run + ' started\n')
 
+            # determine the owner of the script
             script_owner = getpwuid(stat(script_to_run).st_uid).pw_name
+            # construct a cmd to run the script as the owner
             finished_script_cmd = 'sudo su -c \"' + script_to_run + '\"  -s /bin/bash ' + script_owner
-            print(finished_script_cmd)
-
 
             script_log_file = join(self._logs_path, script) + '.log'
             with open(script_log_file, 'w') as script_log:
